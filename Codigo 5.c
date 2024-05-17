0d441d192adea4c78609af4a3b48e264c4f2ab78
MateoDobladez <mdobladez@etrr.edu.ar>
2024-05-17 15:40:26 -0300

    guia de ejercicios

---
+++ Codigo 5.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main (){
+
+    float lado1, lado2, area, perimetro;
+    printf("Ingrese la medida de sus lados en cm:");
+    scanf("%f \n%f", &lado1, &lado2);
+    area = lado1*lado2;
+    perimetro = lado1*2 + lado2*2;
+    printf("El rectangulo tiene un area de %f y un perimetro de %f", area, perimetro)
+
+    return 0;
+}
---
+++ Codigo 6.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+
+    float area, perimetro;
+    printf("Ingrese el valor del area de su cuadrado\n");
+    scanf("%f", &area);
+    lado = sqrt(area);
+    perimetro = lado*4;
+    printf("El perimetro de tu cuadrado es de %f", perimetro);
+
+    return 0;
+
+}
