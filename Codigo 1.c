af92da852d276d222673c4df05a2fde1e2ec3818
MateoDobladez <mdobladez@etrr.edu.ar>
2024-05-17 14:50:22 -0300

    ejercicios de la guia

---
+++ Codigo 1.c
@@ -0,0 +1,18 @@
+//Codigo prueba
+#include <stdio.h>
+
+int main(){
+    
+float cm, ft, inches, var1, var2;
+    var1 = 30.48;
+    var2 = 2.54;
+    printf("Porfavor, introduzca su medida en centimetros\n");
+    scanf("%f", &cm);
+    ft = cm/var1;
+    inches = cm/var2;
+    printf("Su valor en pies es de: %f pies\n", ft);
+    printf("Su valor en pulgadas es de: %f pulgadas", inches)
+    
+    return 0;
+    
+}
