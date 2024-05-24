852d843ebe7d3a0be03588e196c142097da11b20
MateoDobladez <mdobladez@etrr.edu.ar>
2024-05-24 14:53:12 -0300

    cambios2

---
+++ codigo 10.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+
+    int seg, horas, min;
+    printf("ingrese su tiempo en segundos, siempre y cuando su tiempo sea menor a 1 dia\n")
+    scanf("%d", seg);
+    horas = seg/3600;
+    min = (seg%3600)/60;
+    seg = (seg%3600)&60;
+    printf("tu tiempo es de %d horas, %d minutos y %d segundos", horas, min, seg);
+
+    return 0;
+
+}
