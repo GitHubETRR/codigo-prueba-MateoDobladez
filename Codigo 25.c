8a6f3854c195fc46c0c00e9ce8b4c48ffcc30105
MateoDobladez <mdobladez@etrr.edu.ar>
2024-05-29 14:03:04 -0300

    codigo 25

---
+++ Codigo 25.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#define basico = 4500
+
+int main(){
+
+    float horas, ambiguedad, neto, sueldo;
+    printf("años de ambiguedad: ");
+    scanf("%f", &ambiguedad);
+    printf("horas de trabajo por jornada: ");
+    scanf("%f", &horas);
+    sueldo = 4500*horas;
+    neto = sueldo + sueldo*2/100; 
+    printf("Su sueldo neto deberia ser de %f pesos al mes.", neto);
+
+    return 0;
+}
